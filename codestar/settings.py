0e4116f5994df80aaa5788678dd85807da26279d
Robert Beach <surfdemon@gmail.com>
2024-10-24 13:47:55 +0000

    Maint: Change settings for Heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fq$lpat-jf^md98p^dx=&4zr946-(z(6l*6nwd-d&)ox6(ph3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-surfdemon-djangoblog-12gguq8r68b.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-surfdemon-djangoblog-12gguq8r68b.ws.codeinstitute-ide.net', './herokuapp.com']
 
 
 # Application definition
